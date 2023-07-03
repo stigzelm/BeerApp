dc1b386b91192e22628d2b7962eea8ed3abf7329
mstigzelius <martin@agencyleroy.com>
2023-07-03 12:35:40 +0300

    Output all the content we get from the response

--- src/views/Beer/index.tsx
+++ src/views/Beer/index.tsx
@@ -17,9 +17,42 @@ const Beer = () => {
           <h1>{beer?.name}</h1>
         </header>
         <main>
-          <span>
+          <div>
             <b>Type: </b> {beer?.brewery_type}
-          </span>
+          </div>
+          <div>
+            <b>Address: </b> {beer?.address_1}
+          </div>
+          <div>
+            <b>City: </b> {beer?.city}
+          </div>
+          <div>
+            <b>state_province: </b> {beer?.state_province}
+          </div>
+          <div>
+            <b>Postal: </b> {beer?.postal_code}
+          </div>
+          <div>
+            <b>Country: </b> {beer?.country}
+          </div>
+          <div>
+            <b>longitude: </b> {beer?.longitude}
+          </div>
+          <div>
+            <b>latitude: </b> {beer?.latitude}
+          </div>
+          <div>
+            <b>phone: </b> {beer?.phone}
+          </div>
+          <div>
+            <b>website_url: </b> {beer?.website_url}
+          </div>
+          <div>
+            <b>state: </b> {beer?.state}
+          </div>
+          <div>
+            <b>street: </b> {beer?.street}
+          </div>
         </main>
       </section>
     </article>
